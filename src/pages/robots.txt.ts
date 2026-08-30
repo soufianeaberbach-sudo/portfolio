28dd493b6e4657b7c6d9332b8f489b68a1bd5ebe
soufaine aberbach <soufianeaberbach@gmail.com>
2026-08-30 19:45:35 +0100

    refactor: complete final site refinement

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,10 @@
+export const prerender = true;
+
+export function GET({ site }: { site?: URL }) {
+  const origin = site ?? new URL('https://soufianeaberbach.com');
+
+  return new Response(
+    `User-agent: *\nAllow: /\nSitemap: ${new URL('/sitemap.xml', origin)}\n`,
+    { headers: { 'Content-Type': 'text/plain; charset=utf-8' } },
+  );
+}
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,15 @@
+const routes = ['/', '/expertise/', '/portfolio/', '/process/', '/experience/', '/contact/'];
+
+export const prerender = true;
+
+export function GET({ site }: { site?: URL }) {
+  const origin = site ?? new URL('https://soufianeaberbach.com');
+  const urls = routes
+    .map((route) => `  <url><loc>${new URL(route, origin)}</loc></url>`)
+    .join('\n');
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`,
+    { headers: { 'Content-Type': 'application/xml; charset=utf-8' } },
+  );
+}
